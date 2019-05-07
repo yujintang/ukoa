9989af14b583b56784632f19a241a3919601f4eb
flynn.yu <shanquan54@gmail.com>
2019-05-07 16:08:11 +0800

    checkOwnData default enable:false

--- server/mv/checkOwnData.js
+++ server/mv/checkOwnData.js
@@ -2,7 +2,7 @@
  * 检查ownData
  */
 const checkOwnData = options => async (ctx, next) => {
-  const { enable } = Object.assign({ enable: true }, options);
+  const { enable } = Object.assign({ enable: false }, options);
   if (enable && !ctx.app.proxy && !ctx.mergeParams.ownData) throw new Error('必须由api-gateway跳转');
   await next();
 };
