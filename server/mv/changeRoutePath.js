d8e2098daf76c3395fdabd8b2a5f86a93ecd2c91
flynn.yu <shanquan54@gmail.com>
2019-05-14 11:02:10 +0800

    fix changeRoutePath

--- server/mv/changeRoutePath.js
+++ server/mv/changeRoutePath.js
@@ -9,8 +9,12 @@ const changeRoutePath = () => async (ctx, next) => {
   || _.get(ctx, 'request.query.action')
   || _.get(ctx, 'request.query.Action') || '';
   if (ctx.path === '/') {
+    const __view_docs = _.get(ctx, 'request.body.__view_docs') || _.get(ctx, 'request.query.__view_docs') || false;
     const actionSplit = Action.split('.');
-    if (actionSplit.length === 1) ctx.routerPath = `/dynamic/${actionSplit[0]}`;
+    if (actionSplit.length === 1) {
+      const preFix = __view_docs ? 'dynamic' : 'docs';
+      ctx.routerPath = `/${preFix}/${actionSplit[0]}`;
+    }
     if (actionSplit.length === 2) ctx.routerPath = `/proxy/${actionSplit[0]}/${actionSplit[1]}`;
   }
   await next();
