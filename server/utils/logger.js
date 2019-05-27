0f2faaf1fb336484acbaf613a6f4dd94c4d85d19
flynn.yu <flynn.yu@ucloud.cn>
2019-05-27 15:11:22 +0800

    logger data replace \n

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -2,7 +2,7 @@ module.exports = () => {
   const logger = {};
   logger.json = (data) => {
     if (typeof data === 'object') return JSON.stringify(data);
-    return data;
+    return data.replace(/\n/g, ';  ');
   };
   logger.info = data => console.info(logger.json(data));
   logger.error = data => console.error(logger.json(data));
