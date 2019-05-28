726f3602d9765fdecd0c56a7cdfbdb9a9dcbf0f6
flynn.yu <flynn.yu@ucloud.cn>
2019-05-28 20:13:03 +0800

    logger fix

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -10,7 +10,7 @@ module.exports = () => {
     if (typeof data === 'object') data = JSON.stringify(data);
     return data;
   };
-  logger.info = data => console.info(data);
-  logger.error = data => console.error(data);
+  logger.info = data => console.info(logger.json(data));
+  logger.error = data => console.error(logger.json(data));
   return logger;
 };
