43f6d09741dc6f7dd9c5ad761176a4b8bd3aca21
flynn.yu <flynn.yu@ucloud.cn>
2019-09-19 15:11:02 +0800

    fix config online plugins

--- server/plugins/online_config.js
+++ server/plugins/online_config.js
@@ -2,27 +2,38 @@ const assert = require('assert');
 const { get } = require('lodash');
 const path = require('path');
 const fs = require('fs-extra');
+const shortid = require('shortid');
+const moment = require('moment');
 
 // 获取服务器配置
 module.exports = (enable = false, options = {}) => async(ufo) => {
+  let config = {};
   // local config
-  if (!enable) return ufo.config = fs.readJsonSync(path.join(ufo.baseDir, ufo.configDir), { throws: false }) || {};
-  const {
-    url,
-    token,
-    key_path,
-  } = options;
+  if (!enable) {
+    config = fs.readJsonSync(path.join(ufo.baseDir, ufo.configDir), { throws: false }) || {};
+  } else {
+    const { url, token, key_path } = options;
 
-  // 获取线上配置
-  const [consulCfg, err] = await ufo.curl(url, {
-    action: 'ConfigurationDiscovery',
-    token,
-    key_path,
-    dc: 'gd01',
-    recurse: false,
+    // 获取线上配置
+    const [consulCfg, err] = await ufo.curl(url, {
+      action: 'ConfigurationDiscovery',
+      token,
+      key_path,
+      dc: 'gd01',
+      recurse: false,
+    });
+    if (err) process.exit(0);
+    config = get(consulCfg, `configuration[${key_path}]`, {});
+  }
+  ufo.config = Object.assign(config, {
+    ip: ufo.ip,
+    env: ufo.env,
+    version: process.env.CONSUL_VERSION,
+    name: ufo.name,
+    id: shortid.generate(),
+    up_time: moment().format('YYYY-MM-DD HH:mm:ss'),
+    path: ufo.baseDir,
   });
-  if (err) process.exit(0);
-  ufo.config = get(consulCfg, `configuration[${key_path}]`, {});
-  assert(ufo.config.name && ufo.config.version, 'ufo: setup config error, name、version must be exists! ');
-  return true;
+  assert(ufo.config.port, 'ufo: setup config error, port must be exists! ');
+  return ufo.config;
 };
--- server/plugins/registry_service.js
+++ server/plugins/registry_service.js
@@ -1,7 +1,4 @@
 // 注册服务
-const moment = require('moment');
-const api_id = require('shortid').generate();
-
 module.exports = (enable = false, options = {}) => async(ufo) => {
   if (!enable) return;
   const {
@@ -14,18 +11,18 @@ module.exports = (enable = false, options = {}) => async(ufo) => {
     const [, err] = await ufo.curl(url, {
       action: 'ServiceRegistry',
       token,
-      id: api_id,
+      id: ufo.config.id,
       name: ufo.config.name,
       tags: [
         ufo.config.name,
         ufo.env,
         ufo.config.version,
-        moment().format('YYYY-MM-DD HH:mm:ss'),
+        ufo.config.up_time,
       ],
       ip: ufo.ip,
       port: ufo.config.port,
       check_rules: {
-        id: api_id,
+        id: ufo.config.id,
         name: ufo.config.name,
         http: `http://${ufo.ip}:${
           ufo.config.port
